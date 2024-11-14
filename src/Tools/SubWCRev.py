6f5dd1feeefaa55eef3fd237ecde78ebdc3ad914
kfdani <597680745@qq.com>
2024-11-14 02:04:40 +0000

    Reference commt and version modified

--- src/Tools/SubWCRev.py
+++ src/Tools/SubWCRev.py
@@ -260,8 +260,8 @@ class GitControl(VersionControl):
         The first number reflects the number of commits in common with the
         blessed master repository. The second part (separated by " +") reflects
         the number of commits that are different from the master repository"""
-        referencecommit="bd8bee9"
-        referencerevision=28
+        referencecommit="36061cf"
+        referencerevision=59
 
         result = None
         countallfh=os.popen("git rev-list --count %s..HEAD" % \
