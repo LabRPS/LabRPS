906e036c37905620966bb8eb9847c4b67bedca5e
kfdani <597680745@qq.com>
2024-12-14 22:22:13 +0000

    Quantity unit added for KinematicViscosity

--- src/Base/Quantity.cpp
+++ src/Base/Quantity.cpp
@@ -430,6 +430,8 @@ Quantity Quantity::Degree           (1.0           ,Unit(0,0,0,0,0,0,0,1)); // d
 Quantity Quantity::Radian           (180/M_PI      ,Unit(0,0,0,0,0,0,0,1)); // radian
 Quantity Quantity::Gon              (360.0/400.0   ,Unit(0,0,0,0,0,0,0,1)); // gon
 
+Quantity Quantity::KinematicViscosity   (1.0         ,Unit(2,0,-1));// m^2/s
+
 
 
 
