592fb9143a1609dfbd4c30979eac6db466dc400b
kfdani <597680745@qq.com>
2024-12-29 14:19:28 +0000

    QBladePlugin added to SeaLabPlugins

---
+++ src/Mod/SeaLabPlugins/QBladePlugin/QBladePluginGlobal.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *   Copyright (c) 2019 Werner Mayer <wmayer[at]users.sourceforge.net>     *
+ *                                                                         *
+ *   This file is part of the LabRPS development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <RPSGlobal.h>
+
+#ifndef SEALABPLUGIN_GLOBAL_H
+#define SEALABPLUGIN_GLOBAL_H
+
+
+// QBladePlugin
+#ifndef QBladePluginExport
+#ifdef QBladePlugin_EXPORTS
+#  define QBladePluginExport      LABRPS_DECL_EXPORT
+#else
+#  define QBladePluginExport      LABRPS_DECL_IMPORT
+#endif
+#endif
+
+// QBladePluginGui
+#ifndef QBladePluginGuiExport
+#ifdef QBladePluginGui_EXPORTS
+#  define QBladePluginGuiExport   LABRPS_DECL_EXPORT
+#else
+#  define QBladePluginGuiExport   LABRPS_DECL_IMPORT
+#endif
+#endif
+
+#endif //SEALABPLUGIN_GLOBAL_H
