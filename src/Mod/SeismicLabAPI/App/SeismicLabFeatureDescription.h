1064182299f8262ca48f91fcf38979cba8048fca
kfdani <597680745@qq.com>
2025-01-01 01:29:07 +0000

    feature description object updated

--- src/Mod/SeismicLabAPI/App/SeismicLabFeatureDescription.h
+++ src/Mod/SeismicLabAPI/App/SeismicLabFeatureDescription.h
@@ -35,43 +35,49 @@ public:
     SeismicLabFeatureDescription();
     ~SeismicLabFeatureDescription();
 
-	 App::PropertyString PluginName;
-	 App::PropertyString PublicationTitle;
-	 App::PropertyString PublicationLink;
-	 App::PropertyString PublicationAuthor;
-	 App::PropertyString PublicationDate;
-	 App::PropertyString ReleaseDate;
-	 App::PropertyString Author;
-	 App::PropertyString Version;
-	 App::PropertyString APIVersion;
-	 App::PropertyString PluginVersion;
-     App::PropertyString type;
-     App::PropertyString group;
-     App::PropertyBool Stationarity;
-
+	App::PropertyString PluginName;
+	App::PropertyString PublicationTitle;
+	App::PropertyString PublicationLink;
+	App::PropertyString PublicationAuthor;
+	App::PropertyString PublicationDate;
+	App::PropertyString ReleaseDate;
+	App::PropertyString Author;
+	App::PropertyString Version;
+	App::PropertyString APIVersion;
+	App::PropertyString PluginVersion;
+    App::PropertyString type;
+    App::PropertyString group;
+    App::PropertyBool Stationarity;
 	App::PropertyString LabRPSVersion;
 	App::PropertyString Path;
 	App::PropertyString ObjectName;
 	App::PropertyString Description;
+    App::PropertyBool IsUniformModulationFeature;
+    App::PropertyString OutputUnitString;
+    App::PropertyStringList ApplicationFields;
+
 
 	bool operator==(const SeismicLabAPI::SeismicLabFeatureDescription& rhs) const
 	{
-        return PluginName.getValue() == rhs.PluginName.getValue() &&
-			   Description.getValue() == rhs.Description.getValue() &&
-			   PublicationTitle.getValue() == rhs.PublicationTitle.getValue() &&
-			   PublicationLink.getValue() == rhs.PublicationLink.getValue() &&
-			   PublicationAuthor.getValue() == rhs.PublicationAuthor.getValue() &&
-			   PublicationDate.getValue() == rhs.PublicationDate.getValue() &&
-			   ObjectName.getValue() == rhs.ObjectName.getValue() &&
-			   ReleaseDate.getValue() == rhs.ReleaseDate.getValue() &&
-			   Author.getValue() == rhs.Author.getValue() &&
-			   Version.getValue() == rhs.Version.getValue() &&
-			   LabRPSVersion.getValue() == rhs.LabRPSVersion.getValue() &&
-			   APIVersion.getValue() == rhs.APIVersion.getValue() && 
-			   PluginVersion.getValue() == rhs.PluginVersion.getValue() && 
-			   Stationarity.getValue() == rhs.Stationarity.getValue() &&
-			   type.getValue() == rhs.type.getValue() &&
-			   group.getValue() == rhs.type.getValue();
+        return PluginName.getValue() == rhs.PluginName.getValue()
+            && Description.getValue() == rhs.Description.getValue()
+            && PublicationTitle.getValue() == rhs.PublicationTitle.getValue()
+            && PublicationLink.getValue() == rhs.PublicationLink.getValue()
+            && PublicationAuthor.getValue() == rhs.PublicationAuthor.getValue()
+            && PublicationDate.getValue() == rhs.PublicationDate.getValue()
+            && ObjectName.getValue() == rhs.ObjectName.getValue()
+            && ReleaseDate.getValue() == rhs.ReleaseDate.getValue()
+            && Author.getValue() == rhs.Author.getValue()
+            && Version.getValue() == rhs.Version.getValue()
+            && LabRPSVersion.getValue() == rhs.LabRPSVersion.getValue()
+            && APIVersion.getValue() == rhs.APIVersion.getValue()
+            && PluginVersion.getValue() == rhs.PluginVersion.getValue()
+            && Stationarity.getValue() == rhs.Stationarity.getValue()
+            && type.getValue() == rhs.type.getValue()
+            && group.getValue() == rhs.group.getValue()
+            && IsUniformModulationFeature.getValue() == rhs.IsUniformModulationFeature.getValue()
+            && OutputUnitString.getValue() == rhs.OutputUnitString.getValue()
+            && ApplicationFields.getValue() == rhs.ApplicationFields.getValue();
 
 	}
 };
