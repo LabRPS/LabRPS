7274ca3fb775aa9606ba23028018a098d1cfc46f
kfdani <597680745@qq.com>
2025-01-13 23:48:42 +0000

    Conventional spectral representation method added to WindLabPlugin

---
+++ src/Mod/WindLabPlugins/DeodatisAndShinozyka1991Plugin/DeodatisAndShinozyka1991.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+ *   Copyright (c) 2024 Koffi Daniel <kfdani@labrps.com>                   *
+ *                                                                         *
+ *   This file is part of the LabRPS development system.                   *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#ifndef DEODATISANDSHINOZUKA_PLUGIN_SIMUMETHOD_H
+#define DEODATISANDSHINOZUKA_PLUGIN_SIMUMETHOD_H
+
+#include <Mod/WindLabAPI/App/IrpsWLSimuMethod.h>
+
+namespace WindLabAPI { class WindLabSimulationData; }
+
+namespace WindLab {
+
+class CDeodatisAndShinozyka1991 : public WindLabAPI::IrpsWLSimuMethod
+{
+    PROPERTY_HEADER_WITH_OVERRIDE(WindLab::CDeodatisAndShinozyka1991);
+
+public:
+    CDeodatisAndShinozyka1991();
+
+    ~CDeodatisAndShinozyka1991() {};
+
+	bool OnInitialSetting(const WindLabAPI::WindLabSimulationData& Data);
+
+    bool Simulate(const WindLabAPI::WindLabSimulationData& Data, cube &dPhenomenon);
+
+    bool SimulateInLargeScaleMode(const WindLabAPI::WindLabSimulationData& Data, QString &strFileName);
+
+
+};
+
+} // namespace App
+
+#endif // WINDLAB_PLUGIN_SIMUMETHOD_H
---
+++ src/Mod/WindLabPlugins/DeodatisAndShinozyka1991Plugin/Widgets/IconLoader.cpp
@@ -0,0 +1,95 @@
+/* This file is part of AlphaPlot.
+
+   Copyright 2016, Arun Narayanankutty <n.arun.lifescience@gmail.com>
+
+   AlphaPlot is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   AlphaPlot is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with AlphaPlot.  If not, see <http://www.gnu.org/licenses/>.
+
+   Description : AlphaPlot icon loader
+*/
+
+#include "IconLoader.h"
+
+#include <QFile>
+#include <QtDebug>
+
+using namespace WindLab;
+
+int IconLoader::lumen_;
+QList<int> IconLoader::sizes_;
+QList<QString> IconLoader::icon_path_;
+
+void IconLoader::init() {
+  sizes_.clear();
+  sizes_ << 16 << 22 << 24 << 32 << 48 << 64 << 96 << 128;
+  icon_path_.clear();
+  icon_path_ << QString::fromLatin1(":icons/dark")
+             << QString::fromLatin1(":icons/light")
+             << QString::fromLatin1(":icons/common");
+}
+
+QIcon IconLoader::load(const QString& name, const IconMode& iconMode) {
+
+    init();
+
+  QIcon ret;
+  // If the icon name is empty
+  if (name.isEmpty()) {
+    qDebug() << "Icon name is null";
+    return ret;
+  }
+
+  QString filename;
+  switch (iconMode) {
+    case LightDark:
+      (lumen_ < 100) ? filename = icon_path_.at(Dark)
+                     : filename = icon_path_.at(Light);
+      break;
+    case General:
+      filename = icon_path_.at(Indiscriminate);
+      break;
+    default:
+      // should never reach
+      qDebug() << "Unknown IconMode";
+      break;
+  }
+
+  const QString locate(filename + QString::fromLatin1("/%1/%2.%3"));
+  for (int i = 0; i < sizes_.size(); i++) {
+    const QString filename_custom_png(
+        locate.arg(sizes_.at(i)).arg(name).arg(QString::fromLatin1("png")));
+
+    // First check if a png file is available
+    if (QFile::exists(filename_custom_png)) {
+      ret.addFile(filename_custom_png, QSize(sizes_.at(i), sizes_.at(i)));
+    } else {
+      const QString filename_custom_svg(
+          locate.arg(sizes_.at(i)).arg(name).arg(QString::fromLatin1("svg")));
+      // Then check if an svg file is available
+      if (QFile::exists(filename_custom_svg)) {
+        ret.addFile(filename_custom_svg, QSize(sizes_.at(i), sizes_.at(i)));
+      } else {
+        const QString filename_custom_xpm(
+            locate.arg(sizes_.at(i)).arg(name).arg(QString::fromLatin1("xpm")));
+        // Then check if an xpm file is available
+        if (QFile::exists(filename_custom_xpm)) {
+          ret.addFile(filename_custom_xpm, QSize(sizes_.at(i), sizes_.at(i)));
+        }
+      }
+    }
+  }
+
+  // if no icons are found, then...
+  if (ret.isNull()) {
+    qDebug() << "Couldn't load icon" << name;
+  }
+  return ret;
+}
